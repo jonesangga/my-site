0960bb906352f2645a586f566cdfe8b83eee56a4
jonesangga <jonesangga@proton.me>
2024-05-08 07:48:39 +0700

    change tabs to spaces

--- public/js/5-minimal-dark.js
+++ public/js/5-minimal-dark.js
@@ -6,26 +6,26 @@ let title = "Minimal-Dark";
 let pad = 80;
 
 function setup() {
-	let canvas = createCanvas(600, 600);
-	canvas.parent('sketch-container');
-	pixelDensity(density);
-	noLoop();
+    let canvas = createCanvas(600, 600);
+    canvas.parent('sketch-container');
+    pixelDensity(density);
+    noLoop();
 }
 
 function draw() {
-	background('#212121');
-	noStroke();
-	translate(pad, pad);
-	
-	let s = width - 2*pad; // side of initial square
-	let m = 0.9; // ratio of next side and current side
+    background('#212121');
+    noStroke();
+    translate(pad, pad);
+    
+    let s = width - 2*pad; // side of initial square
+    let m = 0.9; // ratio of next side and current side
 
-	while (s > 10) {
-		fill(map(s, 10, width-2*m, 0, 1000));
-		square(0, 0, s);
-		let coor = [[0, 0], [s-s*m, 0], [0, s-s*m], [s-s*m, s-s*m]];
-		let q = random(coor);
-		translate(q[0], q[1]);
-		s *= m;
-	}
+    while (s > 10) {
+        fill(map(s, 10, width-2*m, 0, 1000));
+        square(0, 0, s);
+        let coor = [[0, 0], [s-s*m, 0], [0, s-s*m], [s-s*m, s-s*m]];
+        let q = random(coor);
+        translate(q[0], q[1]);
+        s *= m;
+    }
 }
