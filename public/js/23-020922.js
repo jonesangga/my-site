8f3a95a62e8183882a8d45eb602a3c1cdd26425a
jonesangga <jonesangga@proton.me>
2024-05-09 08:01:33 +0700

    add 5 works to gallery

---
+++ public/js/23-020922.js
@@ -0,0 +1,82 @@
+// jonesangga, 2023
+// github.com/jonesangga
+
+let title = "020922";
+
+let palette = ['#F0D8A8', '#3D1C00', '#86B8B1', '#F2D694', '#FA2A00'];
+
+let pad = 30;
+let scale = 0.95;
+
+function setup() {
+    let canvas = createCanvas(600, 600);
+    canvas.parent('sketch-container');
+    pixelDensity(density);
+    ellipseMode(CORNER);
+    noLoop();
+}
+
+function draw() {
+    background(palette[0]);
+    translate(pad, pad);
+
+    let n = int(random(4, 7));
+    let s = (width-2*pad) / n;
+    let mult = 0.8;
+    let grids = [];
+
+    for (let i = 0; i < n; i++) {
+        grids.push([]);
+        for (let j = 0; j < n; j++) {
+            grids[i].push( new Grid(
+                i*s+(s-s*mult)/2,
+                j*s+(s-s*mult)/2,
+                s*mult
+            ) );
+        }
+    }
+   
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            grids[i][j].display();
+        }
+    }
+}
+
+class Grid {
+    constructor(x, y, d) {
+        this.x = x;
+        this.y = y;
+        this.d = d;
+    }
+    
+    display() {
+        strokeWeight(0.6);
+        let s = this.d;
+        
+        push();
+        translate(this.x, this.y);
+        noStroke();
+        fill(random(palette));
+
+        let o = 10;
+        beginShape();
+        vertex(int(random(-o, o)), int(random(-o, o)));
+        vertex(int(random(-o, o)), s+int(random(-o, o)));
+        vertex(s+int(random(-o, o)), s+int(random(-o, o)));
+        vertex(s+int(random(-o, o)), int(random(-o, o)));
+        endShape(CLOSE);
+
+        let coor;
+        let q;
+        while (s > 10) {
+            fill(random(palette));
+            circle(0, 0, s);
+            coor = [[0, 0], [s-s*scale, 0], [0, s-s*scale], [s-s*scale, s-s*scale]];
+            q = random(coor);
+            translate(q[0], q[1]);
+            s *= scale;
+        }
+        pop();
+    }
+}
---
+++ public/js/23-030922.js
@@ -0,0 +1,85 @@
+// jonesangga, 2023
+// github.com/jonesangga
+
+let title = "030922";
+
+let palette = ['#F0D8A8', '#3D1C00', '#86B8B1', '#F2D694', '#FA2A00'];
+
+let pad = 30;
+let scale = 0.95;
+
+function setup() {
+    let canvas = createCanvas(600, 600);
+    canvas.parent('sketch-container');
+    pixelDensity(density);
+    ellipseMode(CORNER);
+    noLoop();
+}
+
+function draw() {
+    background(palette[0]);
+    translate(pad, pad);
+
+    let n = int(random(2, 5));
+    let s = (width-2*pad) / n;
+    let mult = 0.8;
+    let grids = [];
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (random() < 0.5) {
+                grids.push( new Grid(i*s+(s-s*mult)/2, j*s+(s-s*mult)/2, s*mult) );
+            }
+            else {
+                grids.push(
+                    new Grid(i*s+(s-s*mult)/2, j*s+(s-s*mult)/2, s*mult/2),
+                    new Grid(i*s+(s-s*mult)/2+s/2, j*s+(s-s*mult)/2, s*mult/2),
+                    new Grid(i*s+(s-s*mult)/2, j*s+(s-s*mult)/2+s/2, s*mult/2),
+                    new Grid(i*s+(s-s*mult)/2+s/2, j*s+(s-s*mult)/2+s/2, s*mult/2)
+                )
+            }
+        }
+    }
+
+    for (let i = 0; i < grids.length; i++) {
+        grids[i].display();
+    }
+}
+
+class Grid {
+    constructor(x, y, d) {
+        this.x = x;
+        this.y = y;
+        this.d = d;
+    }
+    
+    display() {
+        strokeWeight(0.6);
+        let s = this.d;
+        
+        push();
+        translate(this.x, this.y);
+        noStroke();
+        fill(random(palette));
+
+        let o = 10;
+        beginShape();
+        vertex(int(random(-o, o)), int(random(-o, o)));
+        vertex(int(random(-o, o)), s+int(random(-o, o)));
+        vertex(s+int(random(-o, o)), s+int(random(-o, o)));
+        vertex(s+int(random(-o, o)), int(random(-o, o)));
+        endShape(CLOSE);
+
+        let coor;
+        let q;
+        while (s > 10) {
+            fill(random(palette));
+            circle(0, 0, s);
+            coor = [[0, 0], [s-s*scale, 0], [0, s-s*scale], [s-s*scale, s-s*scale]];
+            q = random(coor);
+            translate(q[0], q[1]);
+            s *= scale;
+        }
+        pop();
+    }
+}
